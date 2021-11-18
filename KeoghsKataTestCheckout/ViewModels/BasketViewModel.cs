08224dd6cd9e078ae465534ee340ec4079025425
DESKTOP-KMM57K6\serca <your@email.com>
2021-11-18 00:47:06 +0000

    added the viewmodels

---
+++ KeoghsKataTestCheckout/ViewModels/BasketViewModel.cs
@@ -0,0 +1,14 @@
+﻿using KeoghsKataTestCheckout.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KeoghsKataTestCheckout.ViewModels
+{
+    public class BasketViewModel
+    {
+        public List<Product> Products { get; set; }
+        public int TotalPrice { get; set; }
+    }
+}
---
+++ KeoghsKataTestCheckout/ViewModels/ProductViewModel.cs
@@ -0,0 +1,13 @@
+﻿using KeoghsKataTestCheckout.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KeoghsKataTestCheckout.ViewModels
+{
+    public class ProductViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+    }
+}
