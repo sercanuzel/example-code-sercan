7eff4c8c7862416e36c979c81ef3d3fc1737c9e2
DESKTOP-KMM57K6\serca <your@email.com>
2021-11-18 00:46:47 +0000

    added the models to be used

---
+++ KeoghsKataTestCheckout/Models/CustomerBasket.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KeoghsKataTestCheckout.Models
+{
+    public class CustomerBasket
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int CustomerId { get; set; }
+    }
+}
